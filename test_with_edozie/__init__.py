0ff1b130ecc4dfa49656c3b1fc0e36f6378439d3
unknown <chiedoziedavidehirim@gmail.com>
2024-07-25 05:51:20 +0100

    Database

--- test_with_edozie/__init__.py
+++ test_with_edozie/__init__.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Gtrytryterdytr6375865956eby5@$&*^&)(*UHIHBG*$R*&(&)(GBTHTE^U))'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chiedozie:iyoIaNFY3E1KJNJnh6mOaN0lcZkCKH83@dpg-cqg0ufaju9rs73c69g80-a.oregon-postgres.render.com/teset_with_edozie_database'
 
 db = SQLAlchemy(app)
